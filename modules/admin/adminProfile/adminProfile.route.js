e3244538ddd4e999ef8fa84aa9bd4b7b13702cdc
neerajsahni <neerajsahni367@gmail.com>
2024-11-05 20:52:43 +0530

    get-profile api updated

--- modules/admin/adminProfile/adminProfile.route.js
+++ modules/admin/adminProfile/adminProfile.route.js
@@ -2,8 +2,10 @@ const express = require('express');
 const router = express.Router();
 const adminProfileController = require('./adminProfile.controller');
 const validationMiddleware = require('../../../middleware/validationMiddleware');
-const userSchemas = require('../../../validation/userSchema')
+const userSchemas = require('../../../validation/userSchema');
+const { verifyAdmin } = require('../../../middleware/authMiddleware');
 router.post('/', adminProfileController.createAdmin);
+router.use(verifyAdmin);
 router.get('/profile', adminProfileController.getAdminDetails);
 router.put('/update-profile', adminProfileController.updateProfile);
 
